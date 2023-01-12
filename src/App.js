50fa777568bf6444a4bfb260248cb8f5ce1b5fe1
Akash Jadhav <akashjadhav7040253178@gmail.com>
2023-01-13 00:39:25 +0530

    remote

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Inc, Dec } from "./redux";
 
 function App() {
+
+  const dispatch = useDispatch()
+  const counter = useSelector((state) => state)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Redux-Counter</h1>
+      <div className="">
+        <button onClick={ () => dispatch(Inc())} >+</button>
+          <h1>{counter}</h1>
+        <button onClick={() => dispatch(Dec())} >-</button>
+      </div>
     </div>
   );
 }
